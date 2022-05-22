098481d8da7479b6ca92c3c5cd9af90a38e89a08
Agustin_M_Dominguez <agustin.marcelo.dominguez@mi.unc.edu.ar>
2022-05-22 00:01:04 -0300

    Implement CongestionController TAD

--- CongestionController.cc
+++ CongestionController.cc
@@ -8,15 +8,66 @@
 using namespace omnetpp;
 
 CongestionController::CongestionController() {
-	slidingWindow = new pairPacketData[134217727]; // FIXME 134MB
+	slidingWindow = new pairPacketData[1000]; // FIXME 134MB 134217727
 }
 
 CongestionController::~CongestionController() {
 	delete[] slidingWindow;
 }
 
+int CongestionController::getAck(int seqN) {
+	pairPacketData pair = slidingWindow[seqN];
+	return pair->ackCounter;
+}
+
+void CongestionController::addAck(int seqN) {
+	pairPacketData pair = slidingWindow[seqN];
+	(pair->ackCounter)++;
+	slidingWindow[seqN] = pair;
+}
+
+int CongestionController::getThreshold() {
+	return threshold;
+}
+
+void CongestionController::setThreshold(int newValue) {
+	threshold = newValue;
+}
+
+void CongestionController::addVolt(Volt * volt) {
+	int seqN = volt->getSeqNumber();
+	std::cout << "CC :: Adding Volt " << seqN << " to congestion controller\n";
+	pairPacketData newPair = new _pairPacketData();
+	newPair->ackCounter = 0;
+	newPair->volt = volt;
+	slidingWindow[seqN] = newPair;
+}
+
+Volt * CongestionController::popVolt(int seqN) {
+	std::cout << "CC :: Volt " << seqN << " added successfully\n";
+	pairPacketData pair = slidingWindow[seqN];
+	Volt * volt = pair->volt;
+	slidingWindow[seqN] = NULL;
+	delete(pair);
+	return volt;
+}
+
+int CongestionController::getBaseWindow() {
+	return baseWindow;
+}
+
+void CongestionController::setBaseWindow(int base) {
+	std::cout << "CC :: Moving Sliding Window from " << baseWindow << " to " << base << "\n";
+	baseWindow = base;
+}
+
 bool CongestionController::getSlowStart(){
-	return true;
+	return isSlowStartStage;
+}
+
+void CongestionController::setSlowStart(bool state) {
+	isSlowStartStage = state;
 }
 
 #endif /* CONGESTIONCONTROLLER */
+
