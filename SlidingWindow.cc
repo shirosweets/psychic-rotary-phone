58b8d570ada1e86cb9a0e2aff7ec11f4b955c054
Agustin_M_Dominguez <agustin.marcelo.dominguez@mi.unc.edu.ar>
2022-05-29 19:33:02 -0300

    refactor: Fix logging of sliding window

--- SlidingWindow.cc
+++ SlidingWindow.cc
@@ -19,7 +19,7 @@ int SlidingWindow::getAck(int seqN) {
 	windowIterator pairIterator = slidingWindow.find(seqN);
 	if (pairIterator == slidingWindow.end()) {
 		// No existe en el diccionario
-		std::cout << "CC :: WARNING :: getAck(" << seqN << ") didn't found any pairPacketData\n";
+		std::cout << "SW :: WARNING :: getAck(" << seqN << ") didn't found any pairPacketData\n";
 		return -1;
 	}
 	return pairIterator->second->ackCounter;
@@ -30,38 +30,38 @@ void SlidingWindow::addAck(int seqN) {
 	if (pairIterator != slidingWindow.end()) {
 		pairPacketData pair = pairIterator->second;
 		(pair->ackCounter)++;
-		std::cout << "CC :: ackCounter[" << seqN << "]++ " << pair->ackCounter << "\n";
+		std::cout << "SW :: ackCounter[" << seqN << "]++ " << pair->ackCounter << "\n";
 		slidingWindow[seqN] = pair;
 	}
 }
 
 void SlidingWindow::addVolt(Volt * volt) {
 	int seqN = volt->getSeqNumber();
-	std::cout << "CC :: Adding Volt " << seqN << " to congestion controller\n";
+	std::cout << "SW :: Adding Volt " << seqN << " to congestion controller\n";
 	pairPacketData newPair = new _pairPacketData();
 	newPair->ackCounter = 0;
 	newPair->volt = volt;
 	if(slidingWindow.find(seqN) != slidingWindow.end()) {
-		std::cout << "CC :: WARNING :: addVolt is overwriting a previous instance\n";
+		std::cout << "SW :: WARNING :: addVolt is overwriting a previous instance\n";
 	}
 	slidingWindow[seqN] = newPair;
 	bytesInFlight += volt->getByteLength();
-	std::cout << "CC :: !bytesInFlight " << bytesInFlight << "\n";  // FIXME
-	std::cout << "CC :: Volt " << seqN << " added successfully\n";
+	std::cout << "SW :: !bytesInFlight " << bytesInFlight << "\n";  // FIXME
+	std::cout << "SW :: Volt " << seqN << " added successfully\n";
 }
 
 Volt * SlidingWindow::popVolt(int seqN) {
 	Volt * volt = NULL;
 	windowIterator pairIterator = slidingWindow.find(seqN);
 	if (pairIterator != slidingWindow.end()) {
-		std::cout << "CC :: Removing Volt " << seqN << " from congestion controller\n";
+		std::cout << "SW :: Removing Volt " << seqN << " from congestion controller\n";
 		volt = pairIterator->second->volt;
 		delete(pairIterator->second);
 		slidingWindow.erase(pairIterator);
 		bytesInFlight -= volt->getByteLength();
-		std::cout << "CC :: !bytesInFlight " << bytesInFlight << "\n";  // FIXME
+		std::cout << "SW :: !bytesInFlight " << bytesInFlight << "\n";  // FIXME
 	} else {
-		std::cout << "CC :: WARNING :: popVolt could not find Volt " << seqN << "\n";
+		std::cout << "SW :: WARNING :: popVolt could not find Volt " << seqN << "\n";
 	};
 	return volt;
 }
@@ -70,10 +70,10 @@ Volt * SlidingWindow::dupVolt(int seqN) {
 	Volt * volt = NULL;
 	windowIterator pairIterator = slidingWindow.find(seqN);
 	if(pairIterator != slidingWindow.end()) {
-		std::cout << "CC :: Copying Volt " << seqN << "\n";
+		std::cout << "SW :: Copying Volt " << seqN << "\n";
 		volt = pairIterator->second->volt->dup();
 	} else {
-		std::cout << "CC :: WARNING :: dupVolt could not find Volt " << seqN << "\n";
+		std::cout << "SW :: WARNING :: dupVolt could not find Volt " << seqN << "\n";
 	}
 	return volt;
 }
@@ -86,7 +86,7 @@ bool SlidingWindow::isVoltInWindow(int seqN){
 double SlidingWindow::getSendTime(int seqN){
     windowIterator pairIterator = slidingWindow.find(seqN);
     if (pairIterator == slidingWindow.end()) {
-        std::cout << "CC :: WARNING :: getSendTime(" << seqN << ") didn't found any pairPacketData\n";
+        std::cout << "SW :: WARNING :: getSendTime(" << seqN << ") didn't found any pairPacketData\n";
         return -1;
     }
     return pairIterator->second->sendTime;
@@ -106,12 +106,12 @@ int SlidingWindow::getBaseWindow() {
 }
 
 void SlidingWindow::setBaseWindow(int base) {
-	std::cout << "CC :: Moving Sliding Window from " << baseWindow << " to " << base << "\n";
+	std::cout << "SW :: Moving Sliding Window from " << baseWindow << " to " << base << "\n";
 	baseWindow = base;
 }
 
 int SlidingWindow::amountBytesInFlight() {
-	std::cout << "CC :: bytesInFlight " << bytesInFlight << "\n";
+	std::cout << "SW :: " << bytesInFlight << " bytes in flight\n";
 	return bytesInFlight;
 }
 
